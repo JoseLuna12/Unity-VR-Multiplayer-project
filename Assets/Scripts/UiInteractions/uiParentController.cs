f9493d913ed54f4b4266a1cdf6de163562716433
Jose <pepejose@outlook.com>
2019-10-20 23:01:19 -0500

    UI finish
    Ui interactions are finished

---
+++ Assets/Scripts/UiInteractions/uiParentController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uiParentController : MonoBehaviour
+{
+    
+    GameObject GrandParent;
+    Transform[] Childrens;
+    Transform[] allGames;
+    Transform gameOnlyComponents;
+
+    Animator uiAnimator;
+    private bool isOpenLocal;
+
+
+    public bool debugChild;
+
+    void Start()
+    {
+        debugChild = true;
+        GrandParent = this.transform.gameObject;
+
+        uiAnimator = GetComponent<Animator>();
+        isOpenLocal = false;
+
+        if (debugChild)
+            getChildsObject(GrandParent);
+    }
+
+    //here i save the child objects of the UI canvas to an array
+    private void getChildsObject(GameObject gp)
+    {
+        //Extract every child in the canvas
+        //Every child in the canvas represent every interactable thing in canvas
+
+        int children = gp.transform.childCount;
+        Childrens = new Transform[children];
+
+        for (int i = 0; i < children; ++i)
+            Childrens[i] = gp.transform.GetChild(i);
+
+        //Extract children from the Game container
+        //Game Container contain every miniGame representation in the UI
+
+        int gameCardCount = Childrens[0].transform.childCount;
+        allGames = new Transform[gameCardCount];
+
+        for (int h = 0; h < gameCardCount; h++)
+            allGames[h] = Childrens[0].transform.GetChild(h);
+
+        debugChild = false;
+    }
+
+    public void OpenUi(bool Is)
+    {
+        uiAnimator.SetBool("isOpen", Is);
+    }
+
+    public void sortGames(string gameName, bool active)
+    {
+        int gameCardCount = Childrens[0].transform.childCount;
+        for (int i= 0; i< gameCardCount; ++i)
+        {
+            if(allGames[i].transform.gameObject.name == gameName)
+            {
+                Transform onlyGame = allGames[i].transform;
+                int children = onlyGame.transform.childCount;
+                gameOnlyComponents = onlyGame.transform.GetChild(children);
+
+                gameOnlyComponents.gameObject.SetActive(active);
+            }
+        }
+    }
+
+    public void debugVoid()
+    {
+        Debug.Log("Here i called from another script");
+    }
+
+}
---
+++ Assets/Scripts/UiInteractions/uiRaycast.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class uiRaycast : MonoBehaviour
+{
+
+    bool uiPlaySound;
+    public AudioSource uiAudioSource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        uiPlaySound = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.green);
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+
+            if (hit.collider.gameObject.tag == "ui" && Input.GetMouseButtonDown(0) || OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+            {
+                interactableUi(hit.collider.gameObject);
+            }
+        }
+
+
+    }
+
+    public void interactableUi(GameObject Iui)
+    {
+        uiPlaySound = true;
+
+        if (!uiPlaySound || !uiAudioSource.isPlaying)
+        {
+            uiAudioSource.Play();
+            uiPlaySound = false;
+        }
+        else
+        {
+            uiAudioSource.Stop();
+            uiPlaySound = false;
+        }
+
+        GameObject parent;
+        parent = Iui.transform.parent.gameObject;
+        if(Iui.transform.name == "Open")
+        {
+            parent.GetComponent<uiParentController>().OpenUi(true);
+        }else if(Iui.transform.name == "Close")
+        {
+            parent.GetComponent<uiParentController>().OpenUi(false);
+        }
+
+
+
+        if(Iui.transform.name == "Game1")
+        {
+            GameObject GameParent;
+            Transform gameChildren;
+            int childrenCount;
+            GameParent = Iui.transform.parent.gameObject;
+            childrenCount = gameObject.transform.childCount;
+            gameChildren = GameParent.transform.GetChild(childrenCount);
+            gameChildren.transform.gameObject.SetActive(true);
+            SceneManager.LoadScene("BirdLevel");
+        }
+
+        uiPlaySound = false;
+
+    }
+    
+}
